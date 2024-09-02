914a05109a32a3de0cad7a12e6b85a8139f6fce1
Anmol Singhal <anmol.singhal@ucdenver.edu>
2024-09-01 23:43:46 -0600

    changes

--- Concatenation of array.py
+++ Concatenation of array.py
@@ -1,13 +1,17 @@
+"""1929. Concatenation of Array
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n.
+Specifically, ans is the concatenation of two nums arrays. Return the array ans"""
+
 class Solution(object):
     def getConcatenation(self, nums):
         n = len(nums)
         
-        # Constraint checks
+        # Check the Contrains and raise error if needed
         if not (1 <= n <= 1000):
             raise ValueError("Length of nums must be between 1 and 1000.")
         if any(num < 1 or num > 1000 for num in nums):
             raise ValueError("Each element in nums must be between 1 and 1000.")
         
-        # Concatenate nums with itself to form the desired ans array
+        # Concatiniating nums with itself to form the ans array
         ans = nums + nums
         return ans
